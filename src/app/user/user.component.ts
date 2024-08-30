ae0ba33f7674c8ca3c11d0df97413a75a0a279a0
Anand Verma <anandverma552@gmail.com>
2024-08-30 00:09:59 -0400

    Adding user component

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
 
+import { DUMMY_USERS } from '../dummy-users';
+
 @Component({
   selector: 'app-user',
   standalone: true,
-  imports: [],
+  
   templateUrl: './user.component.html',
   styleUrl: './user.component.css'
 })
 export class UserComponent {
+   selectedUser = DUMMY_USERS[0];
+
+   get imagePath(){
+    return `assets/users/${this.selectedUser.avatar}`;
+   }
+
+   onSelectUser(){
+    this.selectedUser = DUMMY_USERS[Math.random() * DUMMY_USERS.length | 0];
+   }
 
 }
