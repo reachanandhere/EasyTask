bf33a54440321704f8c6a324c5ec774df62b370b
Anand Verma <anandverma552@gmail.com>
2024-08-31 01:38:40 -0400

    Migration all components to ngModule

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,14 +1,8 @@
 import { Component, Input } from '@angular/core';
-import { TaskComponent } from './task/task.component';
-import { NgFor } from '@angular/common';
-import { NewTaskComponent } from './new-task/new-task.component';
-import { newTaskData } from './task/task.model';
 import { TasksService } from './tasks.service';
 
 @Component({
   selector: 'app-tasks',
-  standalone: true,
-  imports: [TaskComponent, NewTaskComponent, NgFor],
   templateUrl: './tasks.component.html',
   styleUrl: './tasks.component.css',
 })
