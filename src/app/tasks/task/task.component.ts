cf57d81479a372fc8a5f3bf8f67b7646178d741e
Anand Verma <anandverma552@gmail.com>
2024-08-31 00:29:33 -0400

    removing output

--- src/app/tasks/task/task.component.ts
+++ src/app/tasks/task/task.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { Task } from './task.model';
 import { CardComponent } from '../../shared/card/card.component';
 import { DatePipe } from '@angular/common';
@@ -9,12 +9,12 @@ import { TasksService } from '../tasks.service';
   standalone: true,
   imports: [CardComponent, DatePipe],
   templateUrl: './task.component.html',
-  styleUrl: './task.component.css'
+  styleUrl: './task.component.css',
 })
 export class TaskComponent {
-  @Input({required: true}) task!: Task;
-  @Output() complete = new EventEmitter<string>();
-  constructor(private tasksService:TasksService) {}
+  @Input({ required: true }) task!: Task;
+
+  constructor(private tasksService: TasksService) {}
 
   onCompleteTask() {
     this.tasksService.completeTask(this.task.id);
